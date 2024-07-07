9b538623bbc48a94edd40cb619a221633a0266f5
Johaner <providencesatterfield@gmail.com>
2024-07-08 02:21:00 +0900

    add formik on login page

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,9 +1,16 @@
-import Layout from "@/components/layout/Layout";
 import { Link } from "react-router-dom";
+import { Formik, ErrorMessage } from "formik";
+import * as Yup from "yup";
+import { useTranslation } from "react-i18next";
 
-export default function Home() {
+// components
+import Layout from "@/components/layout/Layout";
+
+export default function () {
+  const { t } = useTranslation();
+
   return (
-    <>
+    <Layout headerStyle={1} footerStyle={1}>
       <div className="tf-section-2 pt-60 widget-box-icon">
         <div className="themesflat-container w920">
           <div className="row">
@@ -17,50 +24,92 @@ export default function Home() {
             </div>
             <div className="col-12">
               <div className="widget-login">
-                <form id="commentform" className="comment-form">
-                  <fieldset className="email">
-                    <label>Email *</label>
-                    <input
-                      type="email"
-                      id="email"
-                      placeholder="mail@website.com"
-                      name="email"
-                      tabIndex={2}
-                      aria-required="true"
-                      required
-                    />
-                  </fieldset>
-                  <fieldset className="password">
-                    <label>Password *</label>
-                    <input
-                      className="password-input"
-                      type="password"
-                      id="password"
-                      placeholder="Min. 8 character"
-                      name="password"
-                      tabIndex={2}
-                      aria-required="true"
-                      required
-                    />
-                    <i
-                      className="icon-show password-addon"
-                      id="password-addon"
-                    />
-                    <div className="forget-password">
-                      <Link href="#">Forget password</Link>
-                    </div>
-                  </fieldset>
-                  <div className="btn-submit mb-30">
-                    <Link
-                      href="/market"
-                      className="tf-button style-1 h50 w-100"
-                      type="submit"
+                <Formik
+                  initialValues={{
+                    email: "",
+                    password: "",
+                  }}
+                  validationSchema={Yup.object().shape({
+                    email: Yup.string().required(
+                      t("errors.form.required", { name: "Email" })
+                    ),
+                    password: Yup.string().required(
+                      t("errors.form.required", { name: "Password" })
+                    ),
+                  })}
+                >
+                  {({ values, handleSubmit, handleChange, handleBlur }) => (
+                    <form
+                      id="commentform"
+                      className="comment-form"
+                      onSubmit={handleSubmit}
                     >
-                      Login
-                      <i className="icon-arrow-up-right2" />
-                    </Link>
-                  </div>
-                </form>
+                      <fieldset className="email">
+                        <label>Email *</label>
+                        <input
+                          type="email"
+                          id="email"
+                          placeholder="mail@website.com"
+                          name="email"
+                          tabIndex={2}
+                          aria-required="true"
+                          required
+                          value={values.email}
+                          onChange={handleChange}
+                          onBlur={handleBlur}
+                        />
+                        <div className="mt-5px">
+                          <ErrorMessage name="email">
+                            {(msg) => (
+                              <span className="text-danger fs-14px">{msg}</span>
+                            )}
+                          </ErrorMessage>
+                        </div>
+                      </fieldset>
+
+                      <fieldset className="password">
+                        <label>Password *</label>
+                        <input
+                          className="password-input"
+                          type="password"
+                          id="password"
+                          placeholder="Min. 8 character"
+                          name="password"
+                          tabIndex={2}
+                          aria-required="true"
+                          required
+                          value={values.password}
+                          onChange={handleChange}
+                          onBlur={handleBlur}
+                        />
+                        <i
+                          className="icon-show password-addon"
+                          id="password-addon"
+                        />
+                        <div className="mt-5px">
+                          <ErrorMessage name="password">
+                            {(msg) => (
+                              <span className="text-danger fs-14px">{msg}</span>
+                            )}
+                          </ErrorMessage>
+                        </div>
+                        <div className="forget-password">
+                          <Link href="#">Forget password</Link>
+                        </div>
+                      </fieldset>
+                      <div className="btn-submit mb-30">
+                        <Link
+                          href="/market"
+                          className="tf-button style-1 h50 w-100"
+                          type="submit"
+                        >
+                          Login
+                          <i className="icon-arrow-up-right2" />
+                        </Link>
+                      </div>
+                    </form>
+                  )}
+                </Formik>
                 <div className="other">or continue</div>
                 <div className="login-other">
                   <Link href="#" className="login-other-item">
@@ -87,6 +136,6 @@ export default function Home() {
           </div>
         </div>
       </div>
-    </>
+    </Layout>
   );
 }
