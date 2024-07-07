c8772182a3ace558793d7cdd616b138b558bde01
Johaner <providencesatterfield@gmail.com>
2024-07-07 13:42:46 +0900

    working on routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import { BrowserRouter } from "react-router-dom";
+
+// components
+import Layout from "./components/layout/Layout";
+import AddClassBody from "./components/elements/AddClassBody";
+import Preloader from "./components/elements/Preloader";
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 1000);
+  }, []);
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <BrowserRouter>
+      {!loading ? (
+        <>
+          <AddClassBody />
+          <Layout headerStyle={1} footerStyle={1}>
+            
+          </Layout>
+        </>
+      ) : (
+        <Preloader />
+      )}
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
---
+++ src/routes.js
@@ -0,0 +1,8 @@
+import Login from "./pages/login";
+
+export default [
+  {
+    path: "/login",
+    component: <Login />,
+  },
+];
