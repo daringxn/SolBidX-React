81e5f08ae6554c63fc10b1c53b551aa757eae25f
daringxn <daringxn@gmail.com>
2025-05-11 01:51:11 +0900

    link app name in header to home page

--- src/components/layout/header/Header3/index.jsx
+++ src/components/layout/header/Header3/index.jsx
@@ -77,14 +77,14 @@ export default function ({ scroll, isMobileMenu, handleMobileMenu }) {
                 <div className="wrap-box flex">
                   <div id="site-logo">
                     <div id="site-logo-inner">
-                      {/* <Link to="/home" rel="home" className="main-logo">
-                        <img
+                      <Link to="/home" rel="home" className="main-logo">
+                        {/* <img
                           id="logo_header"
                           src="/assets/images/logo/logo.png"
                           data-retina="assets/images/logo/logo@2x.png"
-                        />
-                      </Link> */}
-                      <h1>SolBidX</h1>
+                        /> */}
+                        <h1>SolBidX</h1>
+                      </Link>
                     </div>
                   </div>
                   {/* logo */}
@@ -286,14 +286,14 @@ export default function ({ scroll, isMobileMenu, handleMobileMenu }) {
         <div className={`mobile-nav-wrap ${isMobileMenu ? "active" : ""}`}>
           <div className="overlay-mobile-nav" onClick={handleMobileMenu} />
           <div className="inner-mobile-nav">
-            {/* <Link to="/" rel="home" className="main-logo">
-              <img
+            <Link to="/" rel="home" className="main-logo">
+              {/* <img
                 id="mobile-logo_header"
                 src="/assets/images/logo/logo.png"
                 data-retina="/assets/images/logo/logo-dark@2x.png"
-              />
-            </Link> */}
-            <h1>SolBidX</h1>
+              /> */}
+              <h1>SolBidX</h1>
+            </Link>
             <div className="mobile-nav-close" onClick={handleMobileMenu}>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
